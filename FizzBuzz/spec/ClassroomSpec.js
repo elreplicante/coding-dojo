1c6e095edbbb0fce28f28f0780fa933d0d9377d4
Sergio Revilla <sergio@unmundotecnologico.org>
2013-10-12 14:22:20 +0200

    Testing when a class is created with no students

--- FizzBuzz/spec/ClassroomSpec.js
+++ FizzBuzz/spec/ClassroomSpec.js
@@ -1,13 +1,26 @@
 describe("Classroom", function() {
   var classroom;
 
-  beforeEach(function() {
-    classroom = new Classroom(100);
+  describe("when a classroom is built with no students", function() {
+
+  	beforeEach(function() {
+    	classroom = new Classroom();
+  	});
+
+    it("should have 0 students", function() {
+      expect(classroom.students.length).toEqual(0);
+    });
   });
 
   describe("when a classroom is built", function() {
+ 
+    beforeEach(function() {
+    	classroom = new Classroom(100);
+  	});
+
     it("should have the right number of students", function() {
-  	expect(classroom.students.length).toEqual(100);
+  		expect(classroom.students.length).toEqual(100);
   	});
   });
+
 });
--- FizzBuzz/src/Classroom.js
+++ FizzBuzz/src/Classroom.js
@@ -1,4 +1,7 @@
 function Classroom(size) {
+	if (size === null) {
+		size = 0;
+	}
 	this.students = new Array();
 	for (var i = 0; i < size; i++) {
 		this.students[i] = new Student(i);
