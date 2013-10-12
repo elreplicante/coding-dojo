dfe8ee0004c098b1f904564bcc658e04458c825e
Sergio Revilla <sergio@unmundotecnologico.org>
2013-10-12 19:06:31 +0200

    Refactoring grid specs

--- MarsRover/spec/GridSpec.js
+++ MarsRover/spec/GridSpec.js
@@ -1,24 +1,20 @@
 describe("Grid", function() {
 	var grid;
-	var minWidth;
 	
-
 	describe("when a grid is built with no arguments", function() {
 	  
 	  beforeEach(function() {
 	  grid = new Grid();
-	  minWidth = 10;
-	  minHeight = 10;
 	});
 
 	  it("should have at least 10 cells width", function() {
+	  	var minWidth = 10;
 	  	expect(grid.maxWidth).toEqual(minWidth);
 	  });
 
 	  it("should have at least 10 cells height", function() {
+	  	var minHeight = 10;
 	  	expect(grid.maxHeight).toEqual(minHeight);
 	  });
 	});
-
-	
 });
