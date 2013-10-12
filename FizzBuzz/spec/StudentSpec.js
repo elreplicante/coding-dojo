8c77c824921b901ad99405a6bc01dcf82b869f31
Sergio Revilla <sergio@unmundotecnologico.org>
2013-10-12 13:09:38 +0200

    Adding Student a "number" attribute and refactoring tests

--- FizzBuzz/spec/StudentSpec.js
+++ FizzBuzz/spec/StudentSpec.js
@@ -1,19 +1,79 @@
 describe("Student", function(){
 	var student;
-	var number;
 
-	beforeEach(function(){
-		student = new Student();
-	});
-
-	describe("when number is divisible by 3", function(){
+	describe("when student number is divisible by 3", function(){
 		beforeEach(function(){
-			number = 3;
+			student = new Student(3);
 		});
 
 		it("should be Fizz", function() {
-			expect(student.isFizz(number)).toEqual(true);
+			expect(student.isFizz()).toBe(true);
+		});
+
+		it("should not be Buzz", function() {
+			expect(student.isBuzz()).toBe(false);
+		});
+
+		it("should not be FizzBuzz", function() {
+			expect(student.isFizzBuzz()).toBe(false);
 		});
 
 	});
+
+
+	describe("when student number is divisible by 5", function(){
+		beforeEach(function(){
+			student = new Student(5);
+		});
+
+		it("should be Fizz", function() {
+			expect(student.isFizz()).toBe(false);
+		});
+
+		it("should not be Buzz", function() {
+			expect(student.isBuzz()).toBe(true);
+		});
+
+		it("should not be FizzBuzz", function() {
+			expect(student.isFizzBuzz()).toBe(false);
+		});
+	});
+
+	describe("when student number is divisible by 3 and 5", function(){
+		beforeEach(function() {
+			student = new Student(15);
+		});
+
+		it("should not be Fizz", function() {
+			expect(student.isFizz()).toBe(false);
+		});
+
+		it("should not be Buzz", function() {
+			expect(student.isBuzz()).toBe(false);
+		});
+
+		it("should be FizzBuzz", function() {
+			expect(student.isFizzBuzz()).toBe(true);
+		});
+	});
+
+	describe("when student number is not divisible by 3 or by 5", function(){
+		beforeEach(function() {
+			student = new Student(14);
+		});
+
+		it("should not be Fizz", function() {
+			expect(student.isFizz()).toBe(false);
+		});
+
+		it("should not be Buzz", function() {
+			expect(student.isBuzz()).toBe(false);
+		});
+
+		it("should be FizzBuzz", function() {
+			expect(student.isFizzBuzz()).toBe(false);
+		});
+	});
+
+	
 });
--- FizzBuzz/src/Student.js
+++ FizzBuzz/src/Student.js
@@ -1,7 +1,15 @@
-function Student(){
-	
+function Student(number){
+	this.number = number;
 }
 
-Student.prototype.isFizz = function(number) {
-	return number % 3 === 0;
-}
+Student.prototype.isFizz = function() {
+	return this.number % 3 == 0 && this.number % 5 != 0;
+};
+
+Student.prototype.isBuzz = function() {
+	return this.number % 5 == 0 && this.number % 3 != 0;
+};
+
+Student.prototype.isFizzBuzz = function() {
+	return this.number % 5 == 0 && this.number % 3 == 0;
+};
