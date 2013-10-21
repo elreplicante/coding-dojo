7fd461f9d52bed125e2defd9d8a1979bcbe1101d
Sergio Revilla <sergio@unmundotecnologico.org>
2013-10-22 00:22:28 +0200

    Finishing Fizz student tests

--- FizzBuzz/ruby/lib/student.rb
+++ FizzBuzz/ruby/lib/student.rb
@@ -3,5 +3,15 @@ class Student
 		@number = number
 	end
 	
-	
+	def isFizz?
+		@number % 3 == 0
+	end
+
+	def isBuzz?
+		@number % 5 == 0
+	end
+
+	def isFizzBuzz?
+		@number % 3 == 0 && @number % 5 == 0
+	end
 end
--- FizzBuzz/ruby/spec/student_spec.rb
+++ FizzBuzz/ruby/spec/student_spec.rb
@@ -1,10 +1,20 @@
 require "student"
 
 describe Student do
+
   describe "when student number is divisible by 3" do
   	student = Student.new(3)
+
   	it "should be Fizz" do
   	  expect(student.isFizz?).to be_true
+  	end
+
+  	it "should not be Buzz" do
+  	  expect(student.isBuzz?).to be_false
+  	end
+
+  	it "should not be FizzBuzz" do
+  	  expect(student.isFizzBuzz?).to be_false
   	end
   end
 end
