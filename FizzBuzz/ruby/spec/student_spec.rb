66e8ac61ebc977775c99450d86c110c933a35f9f
Sergio Revilla <sergio@unmundotecnologico.org>
2013-10-22 00:56:40 +0200

    Testing Buzz student behavior

--- FizzBuzz/ruby/spec/student_spec.rb
+++ FizzBuzz/ruby/spec/student_spec.rb
@@ -17,4 +17,36 @@ describe Student do
   	  expect(student.isFizzBuzz?).to be_false
   	end
   end
+
+  describe "when student number is divisible by 5" do
+    student = Student.new(5)
+
+  	it "should not be Fizz" do
+  	  expect(student.isFizz?).to be_false
+  	end
+
+  	it "should be Buzz" do
+  	  expect(student.isBuzz?).to be_true
+  	end
+
+  	it "should not be FizzBuzz" do
+  	  expect(student.isFizzBuzz?).to be_false
+  	end
+  end
+
+  describe "when student number is divisible by 3 and 5" do
+    student = Student.new(15)
+
+  	it "should not be Fizz" do
+  	  expect(student.isFizz?).to be_false
+  	end
+
+  	it "should be Buzz" do
+  	  expect(student.isBuzz?).to be_false
+  	end
+
+  	it "should not be FizzBuzz" do
+  	  expect(student.isFizzBuzz?).to be_true
+  	end
+  end
 end
